e06ab304686cceaeffc4d972c5b7c34ea70f4e6c
Geert Jan Bex <geertjan.bex@uhasselt.be>
2018-04-05 18:20:52 +0200

    Add command line argument handling

--- src/server.cpp
+++ src/server.cpp
@@ -1,14 +1,25 @@
 #include <boost/asio/ip/host_name.hpp>
+#include <boost/program_options.hpp>
 #include <iostream>
 #include <zmq.hpp>
 
-int main() {
+#include "worker_ng_config.h"
+
+namespace po = boost::program_options;
+
+using Options = std::tuple<std::string, int>;
+
+Options get_options(int argc, char* argv[]);
+int main(int argc, char* argv[]) {
     const std::string protocol {"tcp"};
-    const std::string port_nr {"5555"};
+    std::string workfile_name;
+    int port_nr;
+    std::tie(workfile_name, port_nr) = get_options(argc, argv);
     auto hostname = boost::asio::ip::host_name();
-    const std::string bind_str {protocol + "://*:" + port_nr};
+    const std::string bind_str {protocol + "://*:" +
+                                std::to_string(port_nr)};
     const std::string info_str {protocol + "://" + hostname +
-                                ":" + port_nr};
+                                ":" + std::to_string(port_nr)};
 
 
     zmq::context_t context(1);
@@ -27,4 +38,45 @@ int main() {
         socket.send(reply);
     }
     return 0;
+}
+
+Options get_options(int argc, char* argv[]) {
+    std::string workfile_name;
+    const int default_port {5555};
+    int port_nr;
+    po::options_description desc("Allowed options");
+    desc.add_options()
+        ("help,h", "produce help message")
+        ("version,v", "show software version")
+        ("workfile", po::value<std::string>(&workfile_name),
+         "work file to use")
+        ("port", po::value<int>(&port_nr)->default_value(default_port),
+         "port to listen on")
+    ;
+    po::positional_options_description pos_desc;
+    pos_desc.add("workfile", -1);
+
+    po::variables_map vm;
+    po::store(po::command_line_parser(argc, argv)
+              .options(desc).positional(pos_desc).run(), vm);
+    po::notify(vm);
+
+    if (vm.count("help")) {
+        std::cout << desc << std::endl;
+        std::exit(0);
+    }
+
+    if (vm.count("version")) {
+        std::cout << worker_ng_NAME << " "
+                  << worker_ng_VERSION_MAJOR << "."
+                  << worker_ng_VERSION_MINOR << std::endl;
+        std::exit(0);
+    }
+
+    if (!vm.count("workfile")) {
+        std::cerr << "### error: no work file specified" << std::endl;
+        std::exit(1);
+    }
+
+    return std::make_tuple(workfile_name, port_nr);
 }
