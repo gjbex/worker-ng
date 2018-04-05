195a944d95713c7f446e0da1545752e62c1f55b7
Geert Jan Bex <geertjan.bex@uhasselt.be>
2018-04-05 18:06:05 +0200

    Add command line option handling

--- src/client.cpp
+++ src/client.cpp
@@ -1,14 +1,27 @@
+#include <boost/program_options.hpp>
 #include <iostream>
 #include <sstream>
+#include <tuple>
 #include <zmq.hpp>
 
-int main() {
-    const int time_out {1000}; // milliseconds
+#include "worker_ng_config.h"
+
+namespace po = boost::program_options;
+
+using Options = std::tuple<std::string, int>;
+
+Options get_options(int argc, char* argv[]);
+
+int main(int argc, char* argv[]) {
+    std::string server;
+    int time_out;
+    std::tie(server, time_out) = get_options(argc, argv);
+
     zmq::context_t context(1);
     zmq::socket_t socket(context, ZMQ_REQ);
     socket.setsockopt(ZMQ_RCVTIMEO, time_out);
     socket.setsockopt(ZMQ_SNDTIMEO, time_out);
-    socket.connect("tcp://localhost:5555");
+    socket.connect(server);
 
     for (int request_nr = 0; request_nr < 5; ++request_nr) {
         zmq::message_t request(4);
@@ -23,4 +36,44 @@ int main() {
         std::cout << "received " << msg_nr << std::endl;
     }
     return 0;
+}
+
+Options get_options(int argc, char* argv[]) {
+    std::string server {""};
+    const int default_timeout {1000};
+    int timeout;
+    po::options_description desc("Allowed options");
+    desc.add_options()
+        ("help,h", "produce help message")
+        ("version,v", "show software version")
+        ("server", po::value<std::string>(&server), "server to use")
+        ("timeout", po::value<int>(&timeout)->default_value(default_timeout),
+         "client time out in ms")
+    ;
+    po::positional_options_description pos_desc;
+    pos_desc.add("server", -1);
+
+    po::variables_map vm;
+    po::store(po::command_line_parser(argc, argv)
+              .options(desc).positional(pos_desc).run(), vm);
+    po::notify(vm);
+
+    if (vm.count("help")) {
+        std::cout << desc << std::endl;
+        std::exit(0);
+    }
+
+    if (vm.count("version")) {
+        std::cout << worker_ng_NAME << " "
+                  << worker_ng_VERSION_MAJOR << "."
+                  << worker_ng_VERSION_MINOR << std::endl;
+        std::exit(0);
+    }
+
+    if (!vm.count("server")) {
+        std::cerr << "### error: no server specified" << std::endl;
+        std::exit(1);
+    }
+
+    return std::make_tuple(server, timeout);
 }
