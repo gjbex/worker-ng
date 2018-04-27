2a215f072eab2b49399e6b73137dceef213c7ad7
Geert Jan Bex <geertjan.bex@uhasselt.be>
2018-04-27 22:19:59 +0200

    Add API documentation

--- src/worker_exception.h
+++ src/worker_exception.h
@@ -3,7 +3,16 @@
 
 namespace worker {
 
+    /*!
+      \brief Exception to be used as base class for all worker exceptions
+     */
     class Worker_exception : public std::exception {
+        /*!
+          \brief Exception constructor.
+          \param message std:string that specifies the specific
+                 inforation about the condition that triggered
+                 the exception.
+         */
         public:
             Worker_exception(const char* msg) : _msg {msg} {};
             virtual char const* what() const throw() { return _msg; };
