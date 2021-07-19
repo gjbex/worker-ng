705e6c670b455b923b6c48a7ef992fb291bfe9f9
Geert Jan Bex <geertjan.bex@uhasselt.be>
2021-07-19 16:24:35 +0200

    Simplify parser implementation

--- src/work_parser/work_parser.cpp
+++ src/work_parser/work_parser.cpp
@@ -10,9 +10,7 @@ namespace worker {
         void Work_parser::parse_next() {
             std::stringstream item {""};
             std::string line;
-            while (std::getline(*ifs_, line)) {
-                if (line == sep_)
-                    break;
+            while (std::getline(*ifs_, line) && line != sep_) {
                 item << line << "\n";
             }
             next_item_ = item.str();
