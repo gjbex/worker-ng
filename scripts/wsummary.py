dfe5d53d4c0908d64176277c5b8296f4c8c9fe19
Geert Jan Bex <geertjan.bex@uhasselt.be>
2021-08-27 16:31:21 +0200

    Add wsummary script

---
+++ scripts/wsummary.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python
+
+import argparse
+import pandas as pd
+from worker.log_parsers import WorkitemLogParser
+import sys
+
+
+SEP = 72*'-'
+
+if __name__ == '__main__':
+    arg_parser = argparse.ArgumentParser(description='parse log file')
+    arg_parser.add_argument('log', help='log file to parse')
+    arg_parser.add_argument('--show_raw', action='store_true',
+                            help='show raw data')
+    arg_parser.add_argument('--show_failed', action='store_true',
+                            help='show workitem IDs of failed items')
+    arg_parser.add_argument('--show_incomplete', action='store_true',
+                            help='show workitem IDs of incomplete items')
+    arg_parser.add_argument('--show_walltime_stats', action='store_true',
+                            help='show statitics on workitem walltime')
+    arg_parser.add_argument('--show_client_stats', action='store_true',
+                            help='show client statistics')
+    arg_parser.add_argument('--show_all', action='store_true',
+                            help='show all information')
+    options = arg_parser.parse_args()
+    parser = WorkitemLogParser()
+    report = parser.parse(options.log)
+    if options.show_raw:
+        print(report.raw)
+        if not options.show_all:
+            sys.exit(0)
+        else:
+            print(SEP)
+    print(f'success: {len(report.successes)}')
+    print(f'failure: {len(report.failures)}')
+    if options.show_failed or options.show_all:
+        print('\t' + ' '.join(str(work_id) for work_id in report.failures))
+    print(f'incomplete: {len(report.incompletes)}')
+    if options.show_incomplete or options.show_all:
+        print('\t' + ' '.join(str(work_id) for work_id in report.incompletes))
+    if options.show_walltime_stats or options.show_all:
+        print(SEP)
+        print('workitem walltime statistics:')
+        for quantity, value in report.walltime_stats.iteritems():
+            print(f'\t{quantity:10s} {value}')
+    if options.show_client_stats or options.show_all:
+        print(SEP)
+        with pd.option_context('display.max_columns', None, "max_rows", None):
+            print(report.client_stats)
