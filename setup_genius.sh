13509a0d689f252074efd164b86bfe7906888f5d
Geert Jan Bex <geertjan.bex@uhasselt.be>
2023-08-04 09:32:58 +0200

    Fix setup scripts for conda initialization

--- setup_genius.sh
+++ setup_genius.sh
@@ -9,6 +9,6 @@ module load Boost/1.76.0-GCC-10.3.0
 export PATH="$(pwd)/distr_genius/bin:${PATH}"
 export LD_LIBRARY_PATH="$(pwd)/distr_genius/lib:${LD_LIBRARY_PATH}"
 
-source "${VSC_DATA}/conf/conda.sh"
+source "${HOME}/.conda_init.sh"
 conda activate worker_ng
 export PYTHONPATH="$(pwd)/scripts:${PYTHONPATH}"
