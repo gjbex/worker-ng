746ba0cd4090242af8359d01da8376cbfc55faa4
Geert Jan Bex <geertjan.bex@uhasselt.be>
2023-11-16 13:28:53 +0100

    Make slurm output parser more robust
    As administrative messages can show up as part of the output of
    sbatch, the parser has to be adapted to deal with it.

--- scripts/worker/slurm/submit_output_parser.py
+++ scripts/worker/slurm/submit_output_parser.py
@@ -2,8 +2,8 @@ import re
 
 
 def parse(output_str):
-    match = re.match(r'Submitted batch job (?P<job_id>\d+) on cluster (?P<cluster>\w+)',
-                     output_str)
+    match = re.search(r'Submitted batch job (?P<job_id>\d+) on cluster (?P<cluster>\w+)',
+                      output_str)
     if match:
         return match['job_id']
     else:
