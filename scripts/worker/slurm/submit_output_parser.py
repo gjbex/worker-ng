bdd74e6a986bc23a170b172d2d51a1a5aa54b69a
Sourcery AI <>
2023-11-17 10:20:26 +0000

    'Refactored by Sourcery'

--- scripts/worker/slurm/submit_output_parser.py
+++ scripts/worker/slurm/submit_output_parser.py
@@ -2,9 +2,10 @@ import re
 
 
 def parse(output_str):
-    match = re.search(r'Submitted batch job (?P<job_id>\d+) on cluster (?P<cluster>\w+)',
-                      output_str)
-    if match:
+    if match := re.search(
+        r'Submitted batch job (?P<job_id>\d+) on cluster (?P<cluster>\w+)',
+        output_str,
+    ):
         return match['job_id']
     else:
         raise ValueError(f'can not parse submission output "{output_str}"')
