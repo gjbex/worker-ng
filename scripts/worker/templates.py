aa3f7adb38675e75ca8f7333c254df92ad1ea793
Geert Jan Bex <geertjan.bex@uhasselt.be>
2024-10-29 13:16:36 +0100

    Adapt module to read from included data

--- scripts/worker/templates.py
+++ scripts/worker/templates.py
@@ -1,6 +1,6 @@
-import importlib.resources
+import pathlib
 
 
 def get_jobscript_template(scheduler_name):
-    package_name = 'worker.' + '_'.join(map(str.lower, scheduler_name.split()))
-    return importlib.resources.read_text(package_name, 'jobscript.tmpl')
+    path = pathlib.Path(__file__).parent / 'worker' / '_'.join(map(str.lower, scheduler_name.split())) / 'jobscript.tmpl'   
+    return path.read_text()
